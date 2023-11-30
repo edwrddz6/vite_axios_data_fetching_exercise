1c3d239d131d4163825829c7432859c6baf3bbdc
Edward Diaz <diazedward24@gmail.com>
2023-11-29 17:34:40 -0800

    Remove .then(data)

--- vite-project/smellyCat.js
+++ vite-project/smellyCat.js
@@ -4,19 +4,18 @@ export function fetchRandomCatGif() {
     const apiURL = "https://cataas.com/cat?json=true";
 
     axios.get(apiURL)
-    .then((response) => response.json())
-    .then((data) => {
-      const catData = data;
-      const catImageContainer = document.getElementById("catImageContainer");
+        .then((response) => {
+            const catData = response.data;
+            const catImageContainer = document.getElementById("catImageContainer");
 
-      if (catData._id) {
-        const catImage = document.createElement("img");
-        catImage.src = `https://cataas.com/cat/${catData._id}`;
-        catImage.alt = "Random Cat";
-        catImageContainer.appendChild(catImage);
-      }
-    })
+            if (catData && catData._id) {
+                const catImage = document.createElement("img");
+                catImage.src = `https://cataas.com/cat/${catData._id}`;
+                catImage.alt = "Random Cat";
+                catImageContainer.appendChild(catImage);
+            }
+        })
         .catch((error) => {
             console.error('Error fetching cat image:', error);
-        })
-}
+        });
+} 
